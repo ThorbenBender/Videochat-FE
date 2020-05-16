f73204ac7630209ee3c7bada4283aac3776b67b6
Thorben <bender.thorben0@gmail.com>
2020-05-16 17:38:12 +0200

    Initialize firebase

--- src/Components/ContextProvider/ContextProvider.js
+++ src/Components/ContextProvider/ContextProvider.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import Context from './Context';
+import * as firebase from 'firebase';
+import firebaseConfig from '../../firebase/firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
 
 const ContextProvider = ({ children }) => {
   const [peer_Connection, setPeer_Connection] = useState(null);
-  return <Context.Provider>{children}</Context.Provider>;
+  const db = firebase.firestore();
+  return <Context.Provider value={{ db: db }}>{children}</Context.Provider>;
 };
 
 export default ContextProvider;
